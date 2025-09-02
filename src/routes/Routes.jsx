13d56db6dbcacdef86106c7d2205a88a1dfc06fa
rmartin-2023205 <rmartin-2023205@kinal.edu.gt>
2025-09-02 11:27:51 -0600

    Se agregan rutas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,26 +1,24 @@
-import { createBrowserRouter as Router, Routes, Route, Link, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { HomePage } from "../pages/HomePage"
 import { GameMode } from "../pages/GameMode"
 import { Layout } from "../components/Layout"
-import NivelFacil from './components/NivelFacil'
-import NivelMedio from './components/NivelMedio'
-import NivelDificil from './components/NivelDificil'
-
+ 
 const router = createBrowserRouter(
     [
         {
             path: "/",
+            element: <HomePage />
+ 
+        },
+        {
+            path: "/game",
             element: <Layout />,
             errorElement: <div>404</div>,
             children: [
                 {
                     index: true,
-                    element: <HomePage />
-                },
-                {
-                    path: "game",
                     element: <GameMode />
-                }
+                },
             ]
         },
         {
@@ -29,26 +27,9 @@ const router = createBrowserRouter(
         }
     ]
 )
-
+ 
 export const MyRouter = () => {
     return (
         <RouterProvider router={router} />
     )
-}
-
-const App = () => {
-  return (
-    <Router>
-      <nav>
-        <Link to="/facil">Fácil</Link>
-        <Link to="/medio">Medio</Link>
-        <Link to="/dificil">Difícil</Link>
-      </nav>
-      <Routes>
-        <Route path="/facil" element={<NivelFacil />} />
-        <Route path="/medio" element={<NivelMedio />} />
-        <Route path="/dificil" element={<NivelDificil />} />
-      </Routes>
-    </Router>
-  )
 }
